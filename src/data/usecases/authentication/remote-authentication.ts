9f68f9382b30b3262ea59d6b9fd40f900d254638
BatistaTony <batistatomeoliveira96@gmail.com>
2022-01-03 11:49:39 +0100

    feat: create a remote authentication

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+
+export class RemoteAuthentication {
+  constructor (
+    private readonly httpPostClient: HttpPostClient,
+    private readonly url: string
+  ) {}
+
+  async auth (): Promise<void> {
+    await this.httpPostClient.post(this.url)
+  }
+}
