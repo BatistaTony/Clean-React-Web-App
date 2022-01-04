96c3ef36a8e614fe9a003eed4d4ac298e6caa079
BatistaTony <batistatomeoliveira96@gmail.com>
2022-01-04 20:15:28 +0100

    feat: test body of the httpPost client

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,5 +1,6 @@
 export type ParamsPosttType = {
   url: string
+  body?: Object
 }
 
 export interface HttpPostClient {
---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,7 @@
+import { AuthParams } from 'domain/usecases/authentication'
+import faker from 'faker'
+
+export const mockAuthentication = (): AuthParams => ({
+  password: faker.internet.email(),
+  email: faker.internet.password()
+})
